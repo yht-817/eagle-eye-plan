e138ce56ee8c34268cc0e6270b3c54dc9a49e76e
yuehongting <1475409096@qq.com>
2018-12-05 17:50:52 +0800

    添加消息控制插件

---
+++ eagle/eagle-message/src/main/java/com/yht/eaglemessage/EagleMessageApplication.java
@@ -0,0 +1,12 @@
+package com.yht.eaglemessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleMessageApplication.class, args);
+    }
+}
